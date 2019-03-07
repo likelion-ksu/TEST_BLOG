05333bae4cdabe5ae66e82f31875d5c4281f8a32
phc5403 <phc5403@gmail.com>
2019-03-08 06:12:39 +0900

    modify settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'qok(=w_a+uvn(^y!7%wa0sn+7z!&k8u_yt24l4$^@^*y4hvi8+'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'qok(=w_a+uvn(^y!7%wa0sn+7z!&k8u_yt24l4$^@^*y4hvi8+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
